f14e590f9348afeb76c3c02c4a15ce8dba49b94c
david-shao318 <d.shao318@gmail.com>
2021-07-14 16:31:57 -0600

    Generate possible numbers given position on board.

--- main.py
+++ main.py
@@ -44,7 +44,34 @@ class Sudoku:
             if i % 3 == 2 and i < 8:
                 print(21 * '-')
 
+    def _check_valid(self, row, col, num):
+        # check entire row
+        for c in range(9):
+            if self._board[row][c] == num:
+                return False
+        # check entire col
+        for r in range(9):
+            if self._board[r][col] == num:
+                return False
+        # check box
+        for r in range((row // 3) * 3, (row // 3) * 3 + 3):
+            for c in range((col // 3) * 3, (col // 3) * 3 + 3):
+                if self._board[r][c] == num:
+                    return False
+        return True
+
+    def possible_numbers(self, row, col):
+        for potential in range(1, 10):
+            if self._check_valid(row, col, potential):
+                yield potential
+
+    def solve(self):
+        return
+
 
 if __name__ == '__main__':
     new_board = Sudoku('board.txt')
     new_board.print_board()
+
+    for n in new_board.possible_numbers(0, 3):
+        print(n)
